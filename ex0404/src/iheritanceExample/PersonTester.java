f287207a1e249cd8616fdf010460e1ef32cde993
gmb65 <bauerg@gmx.net>
2025-01-09 16:53:08 +0100

    push from officemini

---
+++ ex0404/src/iheritanceExample/PersonTester.java
@@ -0,0 +1,16 @@
+package ex0404.src.iheritanceExample;
+
+public class PersonTester {
+
+    public static void main(String[] args){
+
+        Person sally = new Person("Sally", "Phillips");
+        System.out.println(sally);
+
+        Student mike = new Student("Mike", "Thompson", "12345");
+        System.out.println(mike);
+
+        StudentEmployee jeff = new StudentEmployee("Jeff", "Sam", "4567", "#12345", 20.5);
+        System.out.println(jeff);
+    }
+}
---
+++ ex0404/src/iheritanceExample/StudentEmployee.java
@@ -0,0 +1,38 @@
+package ex0404.src.iheritanceExample;
+
+public class StudentEmployee extends Student {
+    private double rateOfPayPerHour;
+    private String employeeId;
+
+    public StudentEmployee(String firstName, String lastName,
+                           String studentId,
+                           String employeeId, double rateOfPayPerHour) {
+        super(firstName, lastName, studentId);
+        this.employeeId = employeeId;
+        this.rateOfPayPerHour = rateOfPayPerHour;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+    public double getRateOfPayPerHour() {
+        return rateOfPayPerHour;
+    }
+
+    public void setRateOfPayPerHour(double rateOfPayPerHour) {
+        this.rateOfPayPerHour = rateOfPayPerHour;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " StudentEmployee{" +
+                "rateOfPayPerHour=" + rateOfPayPerHour +
+                ", employeeId='" + employeeId + '\'' +
+                '}';
+    }
+}
+
